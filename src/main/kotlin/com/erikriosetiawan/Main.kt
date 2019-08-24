187b8ed1c5dcf56606100c08f845983d32463620
erikrios <erikriosetiawan49@gmail.com>
2019-08-24 17:13:28 +0700

    Reading input from command line

--- src/main/kotlin/com/erikriosetiawan/Main.kt
+++ src/main/kotlin/com/erikriosetiawan/Main.kt
@@ -1,5 +1,20 @@
 package com.erikriosetiawan
 
-fun main(vararg args: String) {
-    println("Hello, World!")
+fun main() {
+
+    println("Enter Two  Number")
+    val (a, b) = readLine()!!.split(' ')
+
+    println("Max number is : ${maxNumber(a.toInt(), b.toInt())}")
+}
+
+fun maxNumber(a: Int, b: Int): Int {
+    val max = if (a > b) {
+        println("The value of a is $a")
+        a
+    } else {
+        println("The value of b is $b")
+        b
+    }
+    return max
 }
